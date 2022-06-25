a4b478db74a2e8fa81577c96451ab2509977a1a2
TheAwiteb <awiteb@hotmail.com>
2022-06-25 18:18:24 +0300

    ✨: Delete language function

--- src/cli/sub_commands/delete.rs
+++ src/cli/sub_commands/delete.rs
@@ -19,7 +19,7 @@ use super::utils::Translations;
 use colored::Colorize;
 
 /// Delete the translations
-pub fn delete(i18n_dir: &str, key: &str) {
+pub fn delete_key(i18n_dir: &str, key: &str) {
     match Translations::new(i18n_dir) {
         Ok(mut translations) => {
             if let Err(err) = translations.delete_translation(key) {
@@ -28,6 +28,22 @@ pub fn delete(i18n_dir: &str, key: &str) {
                 err.print();
             } else {
                 println!("'{}' deleted successfully from languages ✅", key.green(),);
+            }
+        }
+        Err(err) => err.print(),
+    }
+}
+
+/// Delete the language
+pub fn delete_language(i18n_dir: &str, lang_name: &str) {
+    match Translations::new(i18n_dir) {
+        Ok(mut translations) => {
+            if let Err(err) = translations.delete_language(lang_name) {
+                err.print();
+            } else if let Err(err) = translations.export() {
+                err.print();
+            } else {
+                println!("'{}' deleted successfully ✅", lang_name.green(),);
             }
         }
         Err(err) => err.print(),
